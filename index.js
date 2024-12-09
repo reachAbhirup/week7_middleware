95abb5e8e54482f6d424abd20b41a7570356b0cf
reachAbhirup <reachme.abhirup@gmail.com>
2024-12-09 12:13:22 +0530

    last_update_middleware

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+const isOldEnoughMiddleware = (req, res, next) => {
+    const userAge = req.query.age;
+
+    if(userAge >= 14){
+        next();
+    } else {
+        res.status(411).json({"Error":"Your age is below minimum age criteria"});
+    }
+}
+
+app.use(isOldEnoughMiddleware);
+
+app.get("/ride1", function(req, res){
+    res.json({"msg":"You have successfully ridden the ride 1"});
+});
+
+app.get("/ride2", function(req, res){
+    res.json({"msg":"You have successfully ridden the ride 2"});
+});
+
+app.listen(5000);
